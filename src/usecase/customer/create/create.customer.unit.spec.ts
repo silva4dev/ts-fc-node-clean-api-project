fa5f40db503336d3cc18179f2f01a5647fa7606d
silva4dev <web4dev.lucas@outlook.com>
2024-03-06 09:38:39 -0300

    test: add create customer unit

---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,58 @@
+import CreateCustomerUseCase from "./create.customer.usecase";
+
+const input = {
+  name: "John Doe",
+  address: {
+    street: "Street",
+    number: 123,
+    zip: "Zip",
+    city: "City",
+  },
+};
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test create customer use case", () => {
+  it("should create a customer", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    const output = await customerCreateUseCase.execute(input);
+
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      address: {
+        street: input.address.street,
+        city: input.address.city,
+        number: input.address.number,
+        zip: input.address.zip,
+      },
+    });
+  });
+
+  it("should throw an error when name is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+    input.name = "";
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Name is required"
+    );
+  });
+
+  it("should throw an error when street is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+    input.address.street = "";
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Street is required"
+    );
+  });
+});
