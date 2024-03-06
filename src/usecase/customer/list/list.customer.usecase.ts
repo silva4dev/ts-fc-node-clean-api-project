166e68aed107c4641f56098800d2a11d4505f9b9
silva4dev <web4dev.lucas@outlook.com>
2024-03-06 19:27:18 -0300

    feat: add list customer use case

---
+++ src/usecase/customer/list/list.customer.usecase.ts
@@ -0,0 +1,38 @@
+import Customer from "../../../domain/customer/entity/customer";
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
+import {
+  InputListCustomerDto,
+  OutputListCustomerDto,
+} from "./list.customer.dto";
+
+export default class ListCustomerUseCase {
+  private customerRepository: CustomerRepositoryInterface;
+
+  constructor(customerRepository: CustomerRepositoryInterface) {
+    this.customerRepository = customerRepository;
+  }
+
+  async execute(input: InputListCustomerDto): Promise<OutputListCustomerDto> {
+    const customers = await this.customerRepository.findAll();
+    return OutputMapper.toOutput(customers);
+  }
+}
+
+class OutputMapper {
+  static toOutput(customer: Customer[]): OutputListCustomerDto {
+    return {
+      customers: customer.map((customer) => {
+        return {
+          id: customer.id,
+          name: customer.name,
+          address: {
+            street: customer.Address.street,
+            city: customer.Address.city,
+            number: customer.Address.number,
+            zip: customer.Address.zip,
+          },
+        };
+      }),
+    };
+  }
+}
