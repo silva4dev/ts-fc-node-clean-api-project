873b704575206089c1247c1af5fc959af25f940f
silva4dev <web4dev.lucas@outlook.com>
2024-03-06 17:58:30 -0300

    refactor: ensure format file

--- src/usecase/customer/create/create.customer.usecase.ts
+++ src/usecase/customer/create/create.customer.usecase.ts
@@ -35,6 +35,6 @@ export default class CreateCustomerUseCase {
         number: customer.Address.number,
         zip: customer.Address.zip,
       },
-    }
+    };
   }
 }
--- src/usecase/customer/find/find.customer.usecase.ts
+++ src/usecase/customer/find/find.customer.usecase.ts
@@ -1,14 +1,17 @@
 import CustomerRepositoryInterface from "../../../domain/customer/repository/customer-repository.interface";
-import { InputFindCustomerDto, OutputFindCustomerDto } from "./find.customer.dto";
+import {
+  InputFindCustomerDto,
+  OutputFindCustomerDto,
+} from "./find.customer.dto";
 
-export default class FindCustomerUseCase {  
+export default class FindCustomerUseCase {
   private customerRepository: CustomerRepositoryInterface;
 
   constructor(customerRepository: CustomerRepositoryInterface) {
     this.customerRepository = customerRepository;
   }
 
-  async execute(input: InputFindCustomerDto): Promise<OutputFindCustomerDto> { 
+  async execute(input: InputFindCustomerDto): Promise<OutputFindCustomerDto> {
     const customer = await this.customerRepository.find(input.id);
     return {
       id: customer.id,
@@ -17,8 +20,8 @@ export default class FindCustomerUseCase {
         street: customer.Address.street,
         city: customer.Address.city,
         number: customer.Address.number,
-        zip: customer.Address.zip
-      }
-    }
+        zip: customer.Address.zip,
+      },
+    };
   }
 }
