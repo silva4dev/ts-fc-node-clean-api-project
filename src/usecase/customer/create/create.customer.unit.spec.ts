33a400823478f161663e2a346fc2b8584d083590
silva4dev <web4dev.lucas@outlook.com>
2024-03-07 08:31:45 -0300

    refactor: ensure rename variable

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -28,9 +28,9 @@ describe("Unit test create customer use case", () => {
 
   it("should create a customer", async () => {
     const customerRepository = MockRepository();
-    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+    const usecase = new CreateCustomerUseCase(customerRepository);
 
-    const output = await customerCreateUseCase.execute(input);
+    const output = await usecase.execute(input);
 
     expect(output).toEqual({
       id: expect.any(String),
@@ -46,19 +46,15 @@ describe("Unit test create customer use case", () => {
 
   it("should throw an error when name is missing", async () => {
     const customerRepository = MockRepository();
-    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+    const usecase = new CreateCustomerUseCase(customerRepository);
     input.name = "";
-    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
-      "Name is required"
-    );
+    await expect(usecase.execute(input)).rejects.toThrow("Name is required");
   });
 
   it("should throw an error when street is missing", async () => {
     const customerRepository = MockRepository();
-    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+    const usecase = new CreateCustomerUseCase(customerRepository);
     input.address.street = "";
-    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
-      "Street is required"
-    );
+    await expect(usecase.execute(input)).rejects.toThrow("Street is required");
   });
 });
