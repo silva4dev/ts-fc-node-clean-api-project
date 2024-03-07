862a27bde16d176a76f268b86dc1984047aaef5b
silva4dev <web4dev.lucas@outlook.com>
2024-03-07 18:33:30 -0300

    test: ensure list all customer

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -36,4 +36,41 @@ describe("E2E test for customer", () => {
     });
     expect(response.status).toBe(500);
   });
+
+  it("should list all customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John Doe",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Mary Doe",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 1234,
+          zip: "123456",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const listResponse = await request(app).get("/customer").send();
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("John Doe");
+    expect(customer.address.street).toBe("Street");
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Mary Doe");
+    expect(customer2.address.street).toBe("Street 2");
+  });
 });
