01549773a5278e657a88a748018547287db5341b
silva4dev <web4dev.lucas@outlook.com>
2024-03-07 08:09:32 -0300

    test: add find product use case unit

---
+++ src/usecase/product/find/find.product.unit.spec.ts
@@ -0,0 +1,36 @@
+import Product from "../../../domain/product/entity/product";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import FindProductUseCase from "./find.product.usecase";
+
+describe("Unit test find customer use case", () => {
+  let product: Product;
+  let MockRepository: () => ProductRepositoryInterface;
+
+  beforeEach(() => {
+    product = new Product("123", "Product 1", 20);
+
+    MockRepository = () => {
+      return {
+        create: jest.fn(),
+        update: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product)),
+        findAll: jest.fn(),
+      };
+    };
+  });
+
+  it("should find a product", async () => {
+    const productRepository = MockRepository();
+    const usecase = new FindProductUseCase(productRepository);
+    const input = {
+      id: "123",
+    };
+    const ouput = {
+      id: "123",
+      name: "Product 1",
+      price: 20,
+    };
+    const result = await usecase.execute(input);
+    expect(result).toEqual(ouput);
+  });
+});
