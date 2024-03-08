1b0c4e0909cfbf9681bc03a7fd6133bfaab408cb
silva4dev <web4dev.lucas@outlook.com>
2024-03-08 15:10:42 -0300

    feat: add methods to change value

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -1,5 +1,9 @@
-export default interface ProductInterface {
+import Entity from "../../@shared/entity/entity.abstract";
+
+export default interface ProductInterface extends Entity {
   get id(): string;
   get name(): string;
   get price(): number;
+  changeName(name: string): void;
+  changePrice(price: number): void;
 }
